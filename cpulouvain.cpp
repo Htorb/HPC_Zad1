c4b2e2ae70263314f52f657535ae2bfa2a96c81e
ambroszczyk <piotr.ambroszczyk@gmail.com>
2020-04-18 14:36:23 +0200

    template

--- cpulouvain.cpp
+++ cpulouvain.cpp
@@ -3,14 +3,54 @@
 #include <fstream>
 #include <algorithm>  
 #include <vector>
+#include <map>
 
+
+// TODO bigger threshold int the first iteration
+// TODO bucket partition
+// TODO two hashmaps; hash function
 
 using namespace std;
 
 using pi = pair<int, int>;
 using tr = pair<pi, float>;
+using vi = vector<int>;
+using vf = vector<float>;
 
 float ITR_MODULARITY_THRESHOLD = 0.1;
+
+template<typename T>
+void head(vector<T> v, int n = 5) {
+    for (int i = 0; i < min(n, (int) v.size()); i++) {
+         cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+template<typename T>
+T sum(vector<T> v) {
+    int sum = 0;
+    for (auto val : v) {
+        sum += val;
+    }
+    return sum;
+}
+
+template<typename T>
+vector<T> cumsum(vector<T> v) {
+    vector<T> cs;
+    int sum = 0;
+    for (auto val : v) {
+        sum += val;
+        cs.push_back(sum);
+    }
+    return cs;
+}
+
+void compute_move(int node, vi& newComm) {
+    map<int, float> hashMap;
+
+}
 
 int main(int argc, char *argv[]) {
     bool show_assignment = false;
@@ -56,9 +96,9 @@ int main(int argc, char *argv[]) {
     sort(tmp.begin(), tmp.end());
 
 
-    vector<int> V(n + 1, 0);
-    vector<int> N(2 * m, 0);
-    vector<float> W(2 * m, 0);
+    vi V(n + 1, 0);
+    vi N(2 * m, 0);
+    vf W(2 * m, 0);
 
     int v_idx = 0;
     for (int i = 0; i < tmp.size(); i++) {
@@ -70,18 +110,44 @@ int main(int argc, char *argv[]) {
     }
     V[v_idx] = 2 * m;
 
-    // for (int i = 0; i < V.size(); i++) {
-    //     cout << V[i] << endl;
-    // }
+
+    vi C(n, 0);
+    for (int i = 0; i < n; ++i) {
+        C[i] = i;
+    }
+
+    float wm = sum(W);
+
+    vf k(n, 0);
+    for (int i = 0; i < n; ++i) {
+        for (int j = V[i]; j < V[i + 1]; ++j) {
+            k[i] += W[j];
+        }
+    }
+
+    vf ac = k;
 
     float old_modularity = 0;
     float new_modularity = 0;
 
+    vi newComm(n,0);
     int itr = 0;
+
     while (itr == 0 || new_modularity - old_modularity > ITR_MODULARITY_THRESHOLD) {
+        for (int i = 0; i < n; ++i) {
+            compute_move(i, newComm);
+        }
+        C = newComm;
+        ac.assign(ac.size(), 0);
+        for (int i = 0; i < n; ++i) {
+            ac[C[i]] += k[i];
+        }
+
 
         itr++;
     }
+
+    head(ac);
 
 
 }
